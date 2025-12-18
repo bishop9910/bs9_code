22f3e3a3d4c6267dfa770b60cf7fc3d3516eaf58
bishop9910 <bishop9910@163.com>
2025-12-18 19:12:36 +0800

    update core

--- bs9_core.py
+++ bs9_core.py
@@ -1,4 +1,11 @@
+from typing import Any
+
 class bishop9910_lib:
   def __init__(self) -> None:
-    self.version: tuple = (0, 0, 1)
+    self.version: tuple[int, int, int] = (0, 0, 1)
     self.author: str = "bishop9910"
+  def __call__(self) -> dict[str, Any]:
+    return {
+      "version": self.version, 
+      "author": self.author
+      }
